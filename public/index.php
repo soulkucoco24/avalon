7b8c918e228a8a650f4a8d30584f01e9c8b4f1ac
xddhaibo <2537070941@qq.com>
2017-02-22 18:10:28 +0800

    更新

--- public/index.php
+++ public/index.php
@@ -1,5 +1,2 @@
 <?php
-use App\Bootstrap;
-include dirname(dirname(__FILE__)) . '/bootstrap/autoload.php';
-$bootstrap = new Bootstrap(dirname(dirname(__FILE__)));
-echo $bootstrap->run();
+echo 'hello world';
--- server/http.php
+++ server/http.php
@@ -1,95 +1,25 @@
 <?php
-class Server
+define('DEBUG', 'on');
+define("WEBPATH", realpath(__DIR__ . '/../'));
+require dirname(__DIR__) . '/vendor/lib_config.php';
+
+//设置PID文件的存储路径
+Swoole\Network\Server::setPidFile(__DIR__ . '/http_server.pid');
+/**
+ * 显示Usage界面
+ * php app_server.php start|stop|reload
+ */
+Swoole\Network\Server::start(function ()
 {
-    private $serv;
-    public $timer_count = 0;
+    $AppSvr = new Swoole\Protocol\HttpServer();
+    $AppSvr->loadSetting(__DIR__.'/swoole.ini'); //加载配置文件
+    $AppSvr->setDocumentRoot(__DIR__.'/../public');
+    $AppSvr->setLogger(new Swoole\Log\EchoLog(true)); //Logger
 
-    public function __construct() {
-        $this->serv = new swoole_server("127.0.0.1", 9501);
-        $this->serv->set([
-            'worker_num' => 8,
-            'task_worker_num' => 2,
-            'daemonize' => false,
+    Swoole\Error::$echo_html = false;
 
-            'log_file' => './swoole_server1.log',
-
-            'heartbeat_check_interval' => 30,   // 每30s检测一次
-            'heartbeat_idle_time' => 60, //60s未传数据则断开
-        ]);
-
-        $this->serv->on('Start', array($this, 'onStart'));
-        $this->serv->on('Connect', array($this, 'onConnect'));
-        $this->serv->on('Receive', array($this, 'onReceive'));
-        $this->serv->on('Close', array($this, 'onClose'));
-
-        $this->serv->on('task', [$this,'onTask']);
-        $this->serv->on('finish', [$this,'onFinish']);
-
-        try {
-            $this->serv->start();
-        } catch (Exception $e) {
-            echo $e->getMessage();exit;
-        }
-        
-    }
-
-    public function onStart( $serv ) {
-        echo "Start\n";
-    }
-
-    public function onConnect( $serv, $fd, $from_id ) {
-        $serv->send( $fd, "Hello {$fd}!" );
-    }
-
-    public function onReceive( swoole_server $serv, $fd, $from_id, $data ) {
-        switch ($data) {
-            case 'timer':
-                $str = "Say ";
-                $time_id = swoole_timer_tick(2000, [$this,'onTimer']);
-                echo " timer: time_id-$time_id\n";
-                break;
-            case 'task':
-                
-                $serv->task($data , -1); 
-                break;
-            case 'db':
-                break;
-            case 'redis':
-                break;
-
-            case 'byebye':
-                $serv->close($fd);
-                break;
-            default:
-                echo "new msg from client[$fd] : $data\n";
-                break;
-        }
-
-        $serv->send($fd, $data);
-    }
-
-    public function onClose( $serv, $fd, $from_id ) {
-        echo "Client {$fd} close connection\n";
-    }
-
-    public function onTimer($timer_id) {
-        echo "tick-2000ms $this->timer_count\n";
-        $this->timer_count++;
-    }
-
-    public function onTask(swoole_server $serv, $task_id, $from_id, $data) {
-        echo "task start with data $data \n";
-        sleep(3);
-        echo "task end\n";
-        return 1;
-    }
-
-    public function onFinish(swoole_server $serv, $task_id, $data) {
-        echo "task on finish \n";
-        return 1;
-    }
-
-}
-
-// 启动服务器
-$server = new Server();
+    $server = Swoole\Network\Server::autoCreate('xdd.cn', 8088);
+    $server->setProtocol($AppSvr);
+    //$server->daemonize(); //作为守护进程
+    $server->run(array('worker_num' => 2, 'max_request' => 5000, 'log_file' => WEBPATH.'/swoole_http.log'));
+});
