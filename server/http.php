e2b8fc9bcf0c55b6e9dd0eaab935b1f93c6a5d7f
xddhaibo <2537070941@qq.com>
2017-02-22 18:29:46 +0800

    更新

--- server/http.php
+++ server/http.php
@@ -1,7 +1,7 @@
 <?php
 define('DEBUG', 'on');
 define("WEBPATH", realpath(__DIR__ . '/../'));
-require dirname(__DIR__) . '/vendor/lib_config.php';
+require dirname(__DIR__) . '/vendor/swoole_web/lib_config.php';
 
 //设置PID文件的存储路径
 Swoole\Network\Server::setPidFile(__DIR__ . '/http_server.pid');
@@ -21,5 +21,5 @@ Swoole\Network\Server::start(function ()
     $server = Swoole\Network\Server::autoCreate('xdd.cn', 8088);
     $server->setProtocol($AppSvr);
     //$server->daemonize(); //作为守护进程
-    $server->run(array('worker_num' => 2, 'max_request' => 5000, 'log_file' => WEBPATH.'/swoole_http.log'));
+    $server->run(array('worker_num' => 2, 'max_request' => 5000, 'log_file' => WEBPATH.'/storage/logs/swoole_http.log'));
 });
