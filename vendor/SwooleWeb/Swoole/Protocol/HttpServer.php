1a6ce6192def4d464b04747b0f9ba7cf94606812
xddhaibo <2537070941@qq.com>
2017-02-28 18:09:41 +0800

    更新

--- vendor/SwooleWeb/Swoole/Protocol/HttpServer.php
+++ vendor/SwooleWeb/Swoole/Protocol/HttpServer.php
@@ -565,7 +565,7 @@ class HttpServer extends Swoole\Protocol\WebServer implements  Swoole\IFace\Prot
             }
             catch (\Exception $e)
             {
-                // $response->setHttpStatus(500);
+                $response->setHttpStatus(500);
                 $response->body .= $e->getMessage() . '!<br /><h1>' . self::SOFTWARE . '</h1>';
             }
             ob_end_clean();
